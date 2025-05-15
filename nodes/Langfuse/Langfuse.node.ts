75175e24eb5cce7d1151550b135ce5091f9c77d7
Evgeny Leonov <eleonov@ronasit.com>
2025-05-15 10:49:39 +0000

    feat: enhance Langfuse node to include Langfuse SDK integration and variable handling

--- nodes/Langfuse/Langfuse.node.ts
+++ nodes/Langfuse/Langfuse.node.ts
@@ -1,4 +1,5 @@
-import { INodeType, INodeTypeDescription, NodeConnectionType } from 'n8n-workflow';
+import { IDataObject, IExecuteFunctions, INodeExecutionData, INodeType, INodeTypeDescription, NodeConnectionType } from 'n8n-workflow';
+import { Langfuse as LangfuseSDK } from 'langfuse';
 
 export class Langfuse implements INodeType {
   description: INodeTypeDescription = {
@@ -48,5 +49,30 @@ export class Langfuse implements INodeType {
         description: 'The variables to be used in the prompt',
       },
     ],
+
+  };
+  async execute(this: IExecuteFunctions): Promise<INodeExecutionData[][]> {
+    const promptName = this.getNodeParameter('name', 0) as string;
+    const variablesRaw = this.getNodeParameter('variables', 0) as Record<string, { name: string, value: string }[]>;
+    const variables = variablesRaw?.assignments.reduce((accumulator, item) => {
+      accumulator[item.name] = item.value;
+      return accumulator;
+    }, {} as Record<string, string>);
+    const credentials = await this.getCredentials('langfuseApi', 0);
+    const returnData: IDataObject[] = [];
+
+
+    const langfuse = new LangfuseSDK({
+      secretKey: credentials.sk as string,
+      publicKey: credentials.pk as string,
+      baseUrl: 'https://cloud.langfuse.com'
+    });
+
+    const prompt = await langfuse.getPrompt(promptName, undefined, { label: 'production' });
+    const compiledChatPrompt = prompt.compile(variables);
+
+    returnData.push({ json: { compiledChatPrompt } });
+
+    return [returnData as INodeExecutionData[]];
   };
 }
