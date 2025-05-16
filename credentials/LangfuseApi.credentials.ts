5da1b6478b277de0e67ba98bd745c59fa84bf120
Alexander Storozhevsky <astorozhevsky@ronasit.com>
2025-05-16 10:03:28 +0000

    feat: add icon for credentials

--- credentials/LangfuseApi.credentials.ts
+++ credentials/LangfuseApi.credentials.ts
@@ -3,9 +3,9 @@ import {
   INodeProperties,
 } from 'n8n-workflow';
 
-
 export class LangfuseApi implements ICredentialType {
   name = 'langfuseApi';
+  icon = { light: 'file:langfuse.svg', dark: 'file:langfuse.svg' } as const
   displayName = 'Langfuse API';
   documentationUrl = 'https://docs.n8n.io/integrations/creating-nodes/build/declarative-style-node/';
   properties: INodeProperties[] = [
