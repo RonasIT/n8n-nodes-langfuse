d0784ad6e357fcc784b23bf970a0ac57865a3eb8
Evgeny Leonov <eleonov@ronasit.com>
2025-05-16 08:39:08 +0000

    fix: update documentation URL comments in LangfuseApi credentials

--- credentials/LangfuseApi.credentials.ts
+++ credentials/LangfuseApi.credentials.ts
@@ -7,11 +7,15 @@ import {
 export class LangfuseApi implements ICredentialType {
   name = 'langfuseApi';
   displayName = 'Langfuse API';
-  // Uses the link to this tutorial as an example
-  // Replace with your own docs links when building your own nodes
   documentationUrl = 'https://docs.n8n.io/integrations/creating-nodes/build/declarative-style-node/';
   properties: INodeProperties[] = [
     {
+      displayName: 'URL',
+      name: 'url',
+      type: 'string',
+      typeOptions: { password: false },
+      default: 'https://cloud.langfuse.com',
+    }, {
       displayName: 'Public Key',
       name: 'pk',
       type: 'string',
